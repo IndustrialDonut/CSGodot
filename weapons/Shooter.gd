705f7151914fc5a66c397241044d1c36029b8c27
JoeW <industrialdonut@gmail.com>
2024-11-26 23:00:25 -0800

    feat: change Shooter to check the reload policy for ammunition ready before shooting

--- weapons/Shooter.gd
+++ weapons/Shooter.gd
@@ -8,13 +8,19 @@ extends Node3D
 ## Collision mask important!
 ## Collides with Areas!
 
+var ammo : ReloadMode
+
 func shoot():
 	# fire hitscan or projectile
-	var collider = $RayCast3D.get_collider()
-	if collider:
-		collider.damage()
-		#print(collider)
-		#print("Dama")
-	# play sound effect
-	# play light effect
-	# emit position on minimap according to noise
+	
+	if ammo.can_shoot():
+		ammo.decrement_ammunition()
+		
+		var collider = $RayCast3D.get_collider()
+		if collider:
+			collider.damage()
+			#print(collider)
+			#print("Dama")
+		# play sound effect
+		# play light effect
+		# emit position on minimap according to noise
