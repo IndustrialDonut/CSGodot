c5007e43c551829c6248337fa0091ae911e4023e
JoeW <industrialdonut@gmail.com>
2025-01-11 22:48:04 -0800

    feat: USP-S config idea..

--- config/weapons_master_config.gd
+++ config/weapons_master_config.gd
@@ -2,6 +2,15 @@ class_name MasterConfig extends Object
 
 
 static var config = {
+	"USP-S" : {
+		"slot_code": Enums.SECONDARY,
+		"firemode": FireMode.SEMI,
+		"special": SuppressedSpecial.new(FireMode.BURST),
+		"mass" : 1.5,
+		"model": "res://models/G18.tscn",
+		"magsize": 15,
+		"magmode": ReloadMode.MAGAZINE,
+	},
 	"G18" : {
 		"slot_code": Enums.SECONDARY,
 		"firemode": FireMode.SEMI,
